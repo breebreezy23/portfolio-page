04fb0cabaea8fbd5a721ef534b58cd02987e6579
Breana Manderson <98623167+breebreezy23@users.noreply.github.com>
2024-06-10 18:51:32 -0400

    added in js to give the overall design and functionality a little push

---
+++ script.js
@@ -0,0 +1,38 @@
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.getElementById('toggleNightMode').addEventListener('click', function () {
+    var body = document.body;
+    body.classList.toggle('night-mode');
+
+    // Check if night mode is active and change the button content
+    if (body.classList.contains('night-mode')) {
+        this.innerHTML = '☾'; // Moon emoji for night mode
+    } else {
+        this.innerHTML = '☼'; // Sun emoji for day mode
+    }
+});
